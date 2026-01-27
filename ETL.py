4635fb10b4e104eaf0f706617ee67e0f406fb49d
PrasannaUK <tallampram@yahoo.com>
2026-01-27 18:28:38 +0000

    Added config

--- ETL.py
+++ ETL.py
@@ -2,13 +2,16 @@
 from pyspark.sql.functions import sum as _sum, count, avg, when, col
 #from Staging import ingest_data, clean_data, load_data #need to package this module
 #from transformed import load_data, enrich_data, aggregate_data, load_data #need to package this module
-#import logging #need to package this module
+#import logging     #need to package this module
 
 raw_path="s3://raw_bucket/transactions.csv"
 staging_path="s3://stage_bucket/clean.csv"
-curated_path="s3://core_bucket/content/transformed.parquet"
+curated_path="s3://core_bucket/transformed.parquet"
+config_path="s3://config_bucket/config.json"
 
 def main():
+  with open(config_path) as f:
+        cfg = json.load(f)
   spark= SparkSession \
        .builder \
        .appName("credit_risk") \
@@ -16,7 +19,7 @@ def main():
   
     #logging.basicConfig(level=logging.INFO)
     #logger = logging.getLogger("DataOpsPipeline")
-
+  
     #logger.info("Pipeline started")
     df_inj=ingest_data(raw_path)
     df_cln=clean_data(df_inj)
@@ -26,8 +29,11 @@ def main():
     df_enrich=enrich_data(df_cln)
     df_aggrt=aggregate_data(df_enrich)
     load_data(df_aggrt,curated_path)
+    #load_to_redshift(df_aggrt, cfg)
 
+    spark.stop()
 
 if __name__ == "__main__":
     main()
-    print("**** ETL has successfully completed for Credit_risk***")
+    print("**** ETL Job has successfully completed for Credit_risk***")
+    #logging.info("**** ETL Job has successfully completed for Credit_risk***")
